f64ca06bef7a3d04a35228699fe1f806bad34041
Jonnie <sfsf0901@gmail.com>
2026-01-18 23:11:54 +0900

    feat: Product Entity, Service, Controller, addProduct API 추가

--- src/main/java/com/ecommerce/project/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/project/repositories/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.ecommerce.project.repositories;
 
-public interface ProductRepository {
+import com.ecommerce.project.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
