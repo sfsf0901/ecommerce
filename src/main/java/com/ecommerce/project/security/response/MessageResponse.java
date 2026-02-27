13215c17b532aec0b785067d3c838a58d8a8a69a
Jonnie <sfsf0901@gmail.com>
2026-02-27 23:29:38 +0900

    feat: Sign up 기능 구현

---
+++ src/main/java/com/ecommerce/project/security/response/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.ecommerce.project.security.response;
+
+import lombok.Data;
+
+@Data
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+}
