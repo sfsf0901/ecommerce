140e39a73c1ffdd06ceab9de69e8af4b5f5aa547
Jonnie <sfsf0901@gmail.com>
2026-03-15 10:03:51 +0900

    feat: 카트 상품 삭제 관련 @Transactional 위치 변경(repository -> service)

--- src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -12,7 +12,6 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
 
     @Modifying
-    @Transactional
     @Query("DELETE FROM CartItem ci WHERE ci.cart.cartId = ?1 AND ci.product.productId = ?2")
     void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
--- src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -185,6 +185,7 @@ public class CartServiceImpl implements CartService {
     }
 
     @Override
+    @Transactional
     public String deleteProductFromCart(Long cartId, Long productId) {
         Cart cart = cartRepository.findById(cartId).orElseThrow(() -> new ResourceNotFoundException("Cart", "cartId", cartId));
 
