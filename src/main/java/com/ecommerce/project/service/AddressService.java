ff64560e6d8638762b0603998204dd69e42721bd
Jonnie <sfsf0901@gmail.com>
2026-03-22 13:02:52 +0900

    chore: 사용하지 않는 import 삭제

--- src/main/java/com/ecommerce/project/service/AddressService.java
+++ src/main/java/com/ecommerce/project/service/AddressService.java
@@ -2,7 +2,6 @@ package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.User;
 import com.ecommerce.project.payload.AddressDTO;
-import jakarta.validation.Valid;
 
 import java.util.List;
 
